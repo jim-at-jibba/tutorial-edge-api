4b80aa09c22abff5c84ecb41bca6627a3843108e
James Best <jim@justjibba.net>
2022-11-03 16:35:48 +0000

    Created db package and connected to db

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/jim-at-jibba/comments-api/internal/db"
+)
 
 // Run - is going to be responsible for
 // instantiation and startup of our app
 func Run() error {
 	fmt.Println("starting our app")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("failed to connect to the database")
+		return err
+	}
+
+	// This is redundent as the Connect methond pings but
+	// for now it means db is used
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+
+	fmt.Println("successfully conected and pinged database")
 	return nil
 }
 func main() {
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/jim-at-jibba/comments-api
 
 go 1.19
+
+require (
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.10.7
+)
