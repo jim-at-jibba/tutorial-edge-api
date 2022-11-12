af82fa66d79ad4af3dd8bc1aea7bc3818d711482
James Best <jim@justjibba.net>
2022-11-12 06:05:34 +0000

    Add get and post handlers

---
+++ internal/transport/http/comment.go
@@ -0,0 +1,64 @@
+package http
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jim-at-jibba/comments-api/internal/comment"
+)
+
+// The NewHandler function expects a services that fullfils the following
+// interface
+type CommentService interface {
+	PostComment(context.Context, comment.Comment) (comment.Comment, error)
+	GetComment(ctx context.Context, ID string) (comment.Comment, error)
+	UpdateComment(ctx context.Context, ID string, newCmt comment.Comment) (comment.Comment, error)
+	DeleteComment(ctx context.Context, ID string) error
+}
+
+func (h *Handler) PostComment(w http.ResponseWriter, r *http.Request) {
+	var cmt comment.Comment
+	if err := json.NewDecoder(r.Body).Decode(&cmt); err != nil {
+		return
+	}
+
+	cmt, err := h.Service.PostComment(r.Context(), cmt)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	if err := json.NewEncoder(w).Encode(cmt); err != nil {
+		panic(err)
+	}
+}
+
+func (h *Handler) GetComment(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	cmt, err := h.Service.GetComment(r.Context(), id)
+	if err != nil {
+		log.Print(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(cmt); err != nil {
+		panic(err)
+	}
+}
+
+func (h *Handler) UpdateComment(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *Handler) DeleteComment(w http.ResponseWriter, r *http.Request) {
+
+}
